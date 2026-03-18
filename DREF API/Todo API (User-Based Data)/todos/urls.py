8aabd3c7becbb6a352801b885b08be5d1a6b8eb3
Joseph_Bio <josephbiossey03@gmail.com>
2026-03-18 10:17:05 +0000

    User Authentication System

--- DREF API/Todo API (User-Based Data)/todos/urls.py
+++ DREF API/Todo API (User-Based Data)/todos/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('',views.api_root, name='api-root'),
+    path('register/', views.UserRegistrationView.as_view(), name='register'),
+    path('login/', views.CustomAuthToken.as_view(), name='login'),
 ]
--- DREF API/Todo API (User-Based Data)/todos/views.py
+++ DREF API/Todo API (User-Based Data)/todos/views.py
@@ -1,5 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .serializers import UserRegistrationSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+
 
 @api_view(['GET'])
 def api_root(request):
@@ -9,3 +15,28 @@ def api_root(request):
                 'todos': '/todos/',
           }
      })
+
+class UserRegistrationView(generics.CreateAPIView):
+     queryset = User.objects.all()
+     serializer_class = UserRegistrationSerializer
+     permission_classes = [permissions.AllowAny]
+
+
+class CustomAuthToken(ObtainAuthToken):
+     permission_classes = [permissions.AllowAny]
+
+     def post(self, request, *args, **kwargs):
+          serializer = self.serializer_class(data=request.data,
+          context={'request': request})
+
+          serializer.is_valid(raise_exception=True)
+          user = serializer.validated_data['user']
+          token, created = Token.objects.get_or_create(user=user)
+          return Response({
+               'token': token.key,
+               'user_id': user.pk,
+               'username': user.username,
+               'email': user.email
+          }
+          )
+
