55d86fe5c05ade1694a720ebde77314d418bbc6e
Joseph_Bio <josephbiossey03@gmail.com>
2026-03-28 07:16:36 +0000

    Fix the syntax error in settings.py

--- DREF API/Todo API (User-Based Data)/backend/settings.py
+++ DREF API/Todo API (User-Based Data)/backend/settings.py
@@ -52,10 +52,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+    ],
 
-    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS' :'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE' : 10,
-    ]
+    
 
 
 }
