f308f801230cb5c5a8b6b0fc186f2553bed811e2
Joseph_Bio <josephbiossey03@gmail.com>
2026-05-12 16:38:38 +0000

    Build the expense model and Expense form and also added the post method form functionality

---
+++ Django/expensetracker/mysite/myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .forms import ExpenseForm
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+    expense_form = ExpenseForm()
+    return render(request, 'myapp/index.html', {'expense_form': expense_form})
