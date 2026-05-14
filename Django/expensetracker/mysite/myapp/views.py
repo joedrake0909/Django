5588e3c027c46ffcb4b0150049265bbb6c689f31
Joseph_Bio <josephbiossey03@gmail.com>
2026-05-14 09:08:37 +0000

    Added the delete functionality and the expense table

--- Django/expensetracker/mysite/myapp/views.py
+++ Django/expensetracker/mysite/myapp/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ExpenseForm
+from .models import Expense
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         expense = ExpenseForm(request.POST)
         if expense.is_valid():
             expense.save()
+    
+    # Get request 
+    expenses = Expense.objects.all()
     expense_form = ExpenseForm()
-    return render(request, 'myapp/index.html', {'expense_form': expense_form})
+    return render(request, 'myapp/index.html', {'expense_form': expense_form, 'expenses': expenses})
+
+
+def edit(request, id):
+    expense = Expense.objects.get(id=id)
+    expense_form = ExpenseForm(instance=expense)
+
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'myapp/edit.html', {'expense_form': expense_form})
+
+def delete(request, id):
+    expense = Expense.objects.get(id=id)
+    if request.method == 'POST':
+        expense.delete()
+    return redirect('index')
+    
