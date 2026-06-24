e25fec3d8efc618ffd8c8af1046c1418b8d71ac1
Joseph_Bio <josephbiossey03@gmail.com>
2026-06-24 14:37:27 +0000

    feat(chat): initialize project and implement chatroom boilerplate
    - Integrated Django Channels and WebSockets architecture
    - Created index page and routing for the chat application
    - Implemented view and logic for creating and listing available chatrooms
    - Created the view structure for individual chatroom sessions

---
+++ Django/Real_Time_Chat_Application/mysite/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import ChatRoom
+
+# Create your views here.
+def index(request):
+    chatrooms = ChatRoom.objects.all()
+    return render(request, 'myapp/index.html', {'chatrooms': chatrooms})
+
+def chatroom(request, slug):
+    chatroom = ChatRoom.objects.get(slug=slug)
+    return render(request, 'myapp/room.html', {'chatroom':chatroom})
